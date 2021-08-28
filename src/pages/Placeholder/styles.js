1729b6ceb7f14a7736cf2fb8007d2b7d62c7b1fd
Nickolay Bulavin <bulavinnik@gmail.com>
2021-08-29 00:31:42 +0400

    Add custom font

--- src/pages/Placeholder/styles.js
+++ src/pages/Placeholder/styles.js
@@ -1,28 +1,28 @@
 import styled, { keyframes } from 'styled-components';
 
 export const AppDiv = styled.div`
-         text-align: center;
+  text-align: center;
 `;
 
 export const AppHeader = styled.header`
 `;
 
 export const AppLink = styled.a`
-         color: #18333c;
+  color: #18333c;
 `;
 
 const rotate = keyframes`
-        from {
-            transform: rotate(0deg);
-        }
+  from {
+    transform: rotate(0deg);
+  }
 
-        to {
-            transform: rotate(360deg);
-        }
+  to {
+    transform: rotate(360deg);
+  }
 `;
 
 export const RotatedLogo = styled.img`
-        height: 40vmin;
-        pointer-events: none;
-        animation: ${rotate} infinite 20s linear;
+  height: 40vmin;
+  pointer-events: none;
+  animation: ${rotate} infinite 20s linear;
 `;
